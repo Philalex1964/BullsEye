8465d3d82e581e0afb5e81ee1cd8e2b729bc0983
Alexander <Philalex196401@gmail.com>
2019-03-26 23:10:41 +0300

    Letter13
    Without webview

---
+++ BullsEye/BullsEye/AboutViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Александр Филиппов on 26.03.2019.
+//  Copyright © 2019 Philalex. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
