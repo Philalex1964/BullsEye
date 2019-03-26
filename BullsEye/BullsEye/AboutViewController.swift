b52ca75dcf0dd617a35d557024da5fd742d27c54
Alexander <Philalex196401@gmail.com>
2019-03-26 23:10:41 +0300

    Letter13
    Without webview

---
+++ BullsEye/BullsEye/AboutViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Александр Филиппов on 26.03.2019.
+//  Copyright © 2019 Philalex. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
